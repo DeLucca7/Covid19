688d38d388c7f35e896d2e589ea8dac21109dddd
Eduardo de Lucca <eduardo.lucca4@gmail.com>
2021-08-23 17:37:45 -0300

    pwa-init

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,35 @@
+import BrazilFlag from '../../assets/images/brasil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import AlemanhaFlag from '../../assets/images/alemanha.png'
+import ItaliaFlag from '../../assets/images/italia.png'
+import UsaFlag from '../../assets/images/usa.png'
+
+const COUNTRIES = [
+    {
+        label: 'Alemanha',
+        value: 'germany',
+        flag: AlemanhaFlag
+    },
+    {
+        label: 'Brasil',
+        value: 'brazil',
+        flag: BrazilFlag
+    },
+    {
+        label: 'China',
+        value: 'china',
+        flag: ChinaFlag
+    },
+    {
+        label: 'Estados Unidos',
+        value: 'usa',
+        flag: UsaFlag
+    },
+    {
+        label: 'Itália',
+        value: 'italy',
+        flag: ItaliaFlag
+    }
+]
+
+export default COUNTRIES
---
+++ src/containers/Main/components/Painel.jsx
@@ -0,0 +1,70 @@
+import React, { memo } from 'react';
+import RefreshIcon from '../../../assets/images/refresh.png';
+import { Card, Typography, Button, Select, MenuItem } from '../../../components';
+import COUNTRIES from '../../../commons/constants/countries'
+import { CardPanelContentStyled, ItemStyled } from './style';
+
+const navigatorHasShare = navigator.share
+
+function Painel({ updateAt, onChange, data, country, getCovidData }) {
+    const { cases, recovered, deaths, todayCases, todayDeaths } = data
+
+    const renderCountries = (country, index) => (
+        <MenuItem key={`country-${index}`} value={country.value}>
+            <ItemStyled>
+                <div>{country.label}</div>
+                <img src={country.flag} width="30px" alt={`País-${country.label}`} />
+            </ItemStyled>
+        </MenuItem>
+    )
+
+    const textCovid19 = `País: ${country} - recuperados: ${recovered}`
+
+    const copyInfo = () => {
+        navigator.clipboard.writeText(textCovid19)
+    }
+
+    const shareInfo = () => {
+        navigator.share({
+            title: `Dados da Covid-19 - ${country}`,
+            text: textCovid19,
+            url: 'https://covid19dio.netlify.app/'
+        })
+    }
+
+    const renderShareButton = (
+        <div>
+            <button variant="contained" color="primary" onClick={shareInfo}>
+                Compartilhar
+            </button>
+        </div>
+    )
+
+    const renderCopyButton = (
+        <div>
+            <Button variant="container" color="primary" onClick={copyInfo}>
+                Copiar
+            </Button>
+        </div>
+    )
+
+    return (
+        <Card>
+            <CardPanelContentStyled>
+                <div>
+                    <Typography variant="h5" components="span" color="primary">COVID-19</Typography>
+                    <Typography variant="h6" components="span" color="primary">Painel Coronavírus</Typography>
+                    <Typography variant="body2" components="span" color="primary">Atualizado em: {updateAt}</Typography>
+                    <div className="pt-2">
+                        <Select onChange={onChange} value={country}>
+                            {COUNTRIES.map(renderCountries)}
+                        </Select>
+                    </div>
+                </div>
+                {navigatorHasShare ? renderShareButton : renderCopyButton}
+            </CardPanelContentStyled>
+        </Card>
+    )
+}
+
+export default memo(Painel)
